ce287e1f1b876bfba80ac3d1c8d6a0d72a5ac310
Natanael Pascal <npascal70@gmail.com>
2026-09-17 13:58:58 +0700

    feat: added edit_news(), delete_news(), and implement tailwind with global css

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,8 @@ from main.views import (
     register,
     login_user,
     logout_user,
+    edit_news,
+    delete_news,
 )
 
 app_name = 'main'
@@ -25,6 +27,7 @@ urlpatterns = [
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name="login"),
-    path('logout/', logout_user, name="logout")
-
+    path('logout/', logout_user, name="logout"),
+    path('news/<uuid:id>/edit/', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete/', delete_news, name='delete_news'),
 ]
--- main/views.py
+++ main/views.py
@@ -70,6 +70,7 @@ def show_main(request):
 
     return render(request, "main.html", context)
 
+@login_required(login_url='/login')
 def create_news(request):
     form = NewsForm(request.POST or None)
 
@@ -82,6 +83,22 @@ def create_news(request):
     context = {'form': form}
 
     return render(request, 'create_news.html', context)
+
+@login_required(login_url='/login')
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+    context = {'form': form}
+    return render(request, 'edit_news.html', context)
+
+@login_required(login_url='/login')
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
 
 @login_required(login_url='/login')
 def show_news(request, id):
